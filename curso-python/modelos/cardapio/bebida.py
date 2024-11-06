ea6e656cde1da3d58683f722ca1823ff4cec4c10
Bruno Oliveira <dosantosbru@icloud.com>
2024-11-06 20:02:25 -0300

    python o-o, tópico 1 - herança

---
+++ curso-python/modelos/cardapio/bebida.py
@@ -0,0 +1,5 @@
+class Bebida:
+    def __init__(self, nome, preco, tamanho):
+        self._nome = nome.title()
+        self._preco = preco
+        self._tamanho = tamanho
---
+++ curso-python/modelos/cardapio/prato.py
@@ -0,0 +1,4 @@
+class Prato:
+    def __init__(self, nome, preco,descricao):
+        self._nome = nome.title()
+        self._preco = preco
